22e0236e64c5c50ecc4b912e2f5b126e81d2458f
Franco <franco.mcarballo@outlook.com>
2020-10-04 20:07:56 -0600

    Statics (#2)
    * Add Static Views and Controllers
    
    * Title in Layout, New Header partial

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,7 +1,14 @@
 class StaticsController < ApplicationController
+
   def contact
+    @id = params['id']
+    @pagina = params['pagina']
   end
 
   def us
+    @validador = true
+    @usuarios = ['Franco', 'Leo', 'Pepillo']
+    @mensaje = "Hola desde el controlles"
   end
+
 end
