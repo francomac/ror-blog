c5140a83f9780908297fb5a633fc498144307cec
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2020-10-07 23:08:34 -0600

    Add Articles Controller, Views, and Resources

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,30 @@
+class ArticlesController < ApplicationController
+  def index
+    #show a list of record
+  end
+
+  def show
+    # show a record
+  end
+
+  def new
+    # form to create
+  end
+
+  def create
+    # action for new record, executed by action 'new'
+  end
+
+  def edit
+    # form to edit
+  end
+
+  def update
+    # action to update, executed by action 'edit'
+  end
+
+  def destroy
+    # action to delete
+  end
+
+end
