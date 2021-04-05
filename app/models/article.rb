ab4f4f53b877bbd0495a90f241fa7a8de25fe573
Franco Martinez Carballo <franco.mcarballo@outlook.com>
2021-04-04 21:50:01 -0600

    errors management for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :author
+  validates :titulo, presence: true
+  validates :contenido, presence:  {message: "texto es requerido"}, length: {minimum: 50, message: "texto muy corto (minimo 50 caracteres)"}
 end
